b5200fc38f62430a4d88aafcf35cbad934233a89
Ashfaq Farooqui <ashfaq.farooqui@gmail.com>
2017-02-19 20:57:58 +0100

    Working on a new config

--- emacsConf/.emacs.d/init.el
+++ emacsConf/.emacs.d/init.el
@@ -1,5 +1,4 @@
 
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -8,7 +7,7 @@
 
 (require 'org-install)
 (require 'ob-tangle)
-(org-babel-load-file (expand-file-name "ashfaq.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "newConfig.org" user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,7 +15,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (guide-key org-bullets helm evil projectile dumb-jump ido-grid-mode yafolding rainbow-delimiters monokai-theme neotree expand-region window-numbering ace-jump-mode find-file-in-repository multiple-cursors ensime yaml-mode writegood-mode web-mode solarized-theme smex powerline marmalade markdown-mode magit htmlize haml-mode graphviz-dot-mode flycheck engine-mode feature-mode f ess autopair ac-slime))))
+    (zenburn-theme zenburn yaml-mode yafolding writegood-mode window-numbering which-key web-mode use-package solarized-theme smex smartparens rainbow-delimiters projectile powerline org-bullets neotree multiple-cursors monokai-theme marmalade markdown-mode magit ido-grid-mode htmlize helm haml-mode guide-key graphviz-dot-mode flycheck find-file-in-repository feature-mode expand-region evil ess ensime engine-mode dumb-jump doom-themes autopair ace-jump-mode ac-slime))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
