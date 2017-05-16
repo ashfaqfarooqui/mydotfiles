fe89dfa15eee701b9844fd86d0ab2d54066005f9
Ashfaq Farooqui <ashfaq.farooqui@gmail.com>
2017-05-16 14:11:31 +0200

    added ledger config

--- emacsConf/.emacs.d/init.el
+++ emacsConf/.emacs.d/init.el
@@ -16,7 +16,8 @@
  '(latex-preview-pane-multifile-mode (quote auctex))
  '(package-selected-packages
    (quote
-    (zenburn-theme zenburn yaml-mode yafolding writegood-mode window-numbering which-key web-mode use-package solarized-theme smex smartparens rainbow-delimiters projectile powerline org-bullets neotree multiple-cursors monokai-theme marmalade markdown-mode magit ido-grid-mode htmlize helm haml-mode guide-key graphviz-dot-mode flycheck find-file-in-repository feature-mode expand-region evil ess ensime engine-mode dumb-jump doom-themes autopair ace-jump-mode ac-slime))))
+    (flycheck-ledger zenburn-theme zenburn yaml-mode yafolding writegood-mode window-numbering which-key web-mode use-package solarized-theme smex smartparens rainbow-delimiters projectile powerline org-bullets neotree multiple-cursors monokai-theme marmalade markdown-mode magit ido-grid-mode htmlize helm haml-mode guide-key graphviz-dot-mode flycheck find-file-in-repository feature-mode expand-region evil ess ensime engine-mode dumb-jump doom-themes autopair ace-jump-mode ac-slime)))
+ '(pdf-latex-command "pdflatex"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
