590e864ff4e2eaa10d4f1476784b638fd0e1ddfd
Ashfaq Farooqui <ashfaq@ashfaqfarooqui.me>
2020-09-11 16:12:56 +0200

    spellcheck

--- doomEmacs/.doom.d/init.el
+++ doomEmacs/.doom.d/init.el
@@ -88,7 +88,10 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       spell             ; tasing you for misspelling mispelling
+       (spell
+        +flyspell
+        +everywhere
+        +aspell)             ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
