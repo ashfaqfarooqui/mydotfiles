5ddcbb9cdbfb8b48a689924eeb24e4fd0ba54686
Ashfaq Farooqui <ashfaq@ashfaqfarooqui.me>
2020-10-22 08:32:47 +0200

    ...

--- doomEmacs/.doom.d/init.el
+++ doomEmacs/.doom.d/init.el
@@ -35,7 +35,7 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;fill-column       ; a `fill-column' indicator
+       fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        (hydra +hydra/window-nav/body
               +hydra/text-zoom/body)
@@ -50,7 +50,7 @@
        (ligatures +extra)
        ;;tabs              ; an tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
-       ;unicode           ; extended unicode support for various languages
+       unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
@@ -161,6 +161,7 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
+        +hugo
         +journal
         +pretty
         +pandoc
