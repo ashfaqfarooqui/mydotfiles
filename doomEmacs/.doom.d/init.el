a5b576a8236278082febe8c1e5950099415df3b2
Ashfaq Farooqui <ashfaq@ashfaqfarooqui.me>
2020-11-26 20:02:34 +0100

    packages

--- doomEmacs/.doom.d/init.el
+++ doomEmacs/.doom.d/init.el
@@ -21,21 +21,23 @@
        :completion
        (company
         +childframe)                                     ; the ultimate code completion backend
-       ;;helm              ; the *other* search engine for love and life
+       ;(helm
+       ; +childframe)              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy
-        +prescient
-        ;;+fuzzy
-        +childframe
-        +icons
-        )               ; a search engine for love and life
+        (ivy
+         +prescient
+         ;;+fuzzy
+         +childframe
+         +icons
+         )
+                                        ; a search engine for love and life
 
        :ui
        deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;fill-column       ; a `fill-column' indicator
+       fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        (hydra +hydra/window-nav/body
               +hydra/text-zoom/body)
