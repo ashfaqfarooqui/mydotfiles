887f1589e3861fe282f87139d5453ac7863d6c95
Ashfaq Farooqui <ashfaq@ashfaqfarooqui.me>
2024-10-09 12:17:47 +0200

    adding tmux, nvim and kitty

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+-- For conciseness
+local opts = { noremap = true, silent = true }
+-- Vertical scroll and center
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+
+-- Find and center
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
+
+vim.keymap.set("n", "C-h", ":TmuxNavigateLeft<CR>")
+vim.keymap.set("n", "C-j", ":TmuxNavigateDown<CR>")
+vim.keymap.set("n", "C-k", ":TmuxNavigateUp<CR>")
+vim.keymap.set("n", "C-l", ":TmuxNavigateRight<CR>")
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+--
+
+vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
+vim.o.undofile = true -- Save undo history (default: false)
+vim.o.wrap = true
