e75fa3561fbd49b1ed87206560952ac70a141fbf
Ashfaq Farooqui <ashfaq@ashfaqfarooqui.me>
2024-12-27 21:55:13 +0100

    updates

---
+++ install/nvim.sh
@@ -0,0 +1,14 @@
+cd /tmp
+wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
+tar -xf nvim.tar.gz
+sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
+sudo cp -R nvim-linux64/lib /usr/local/
+sudo cp -R nvim-linux64/share /usr/local/
+rm -rf nvim-linux64 nvim.tar.gz
+cd -
+
+# Replace desktop launcher with one running inside Alacritty
+if [[ -d ~/.local/share/applications ]]; then
+  sudo rm -rf /usr/share/applications/nvim.desktop
+  source ~/mydotfiles/applications/nvim.sh
+fi
