3704d759b00b7fabd5820caa0038607dc245cf4f
Ashfaq Farooqui <ashfaq@ashfaqfarooqui.me>
2024-12-27 15:45:11 +0100

    updates

---
+++ scripts/install/changeShell.sh
@@ -0,0 +1,7 @@
+# Install ohmyzsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
+
+#install starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
