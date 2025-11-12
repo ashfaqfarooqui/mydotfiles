b72639f1973819551d20c0323739641a838f462f
Ashfaq Farooqui <ashfaq@ashfaqfarooqui.me>
2025-11-12 18:20:55 +0100

    base zsh stuff

--- scripts/install/changeShell.sh
+++ scripts/install/changeShell.sh
@@ -6,6 +6,7 @@ chsh -s $(which zsh)
 #install starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
-git clone https://github.com/fdellwing/zsh-bat.git $ZSH_CUSTOM/plugins/zsh-bat
+git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
