cee2561fb8254638943cfd1e0c7231f6208764f2
Ashfaq Farooqui <ashfaq@ashfaqfarooqui.me>
2025-11-13 12:47:31 +0100

    packages

---
+++ scripts/install/setup.sh
@@ -0,0 +1,94 @@
+yay -S stow --needed
+yay -S zsh --needed
+yay -S waybar --needed
+yay -S bat --needed
+yay -S btop --needed
+yay -S chromium --needed
+yay -S fastfetch --needed
+yay -S git --needed
+yay -S gpg --needed
+yay -S neovim --needed
+yay -S opencode-bin --needed
+yay -S rofi --needed
+yay -S starship --needed
+yay -S swaync --needed
+yay -S tmux --needed
+yay -S vscode --needed
+yay -S waybar --needed
+yay -S wlogout --needed
+
+yay -S zen-browser-bin --needed
+yay -S zotero-bin --needed
+yay -S zoxide --needed
+yay -S eza --needed
+
+yay -S obsidian --needed
+yay -S curl --needed
+# rust
+#curl --proto '=https' --tlsv1.2 https://sh.rustup.rs | sh
+#curl -LsSf https://astral.sh/uv/install.sh | sh
+#curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | shcurl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+
+yay -S teams-for-linux --needed
+yay -S uv --needed
+
+yay -S ttf-jetbrains-mono-nerd --needed
+
+yay -S otf-font-awesome ttf-fira-code ttf-fira-sans ttf-firacode-nerd --needed
+
+yay -S bluetui --needed
+yay -S bitwarden-cli --needed
+yay -S rofi-rbw --needed
+yay -S hyprpaper --needed
+yay -S hyprlock --needed
+yay -S hypridle --needed
+yay -S noto-fonts --needed
+yay -S brightnessctl --needed
+yay -S xdg-desktop-portal-hyperland --needed
+yay -S libnotify --needed
+yay -S wlogout --needed
+yay -S hyprsunset --needed
+yay -S unzip --needed
+yay -S fastfetch --needed
+yay -S network-manager-applet --needed
+yay -S gtk4 --needed
+yay -S jg --needed
+yay -S blueman --needed
+yay -S cliphist --needed
+yay -S nwg-look --needed
+yay -S hyprshade --needed
+
+yay -S wireguard-tools --needed
+
+yay -S amazingmarvin-appimage --needed
+yay -S amdgpu_top --needed
+yay -S fd --needed
+yay -S dust --needed
+yay -S duf --needed
+yay -S hyprpolkitagent
+#yay -S xwaylandvideobridge --needed
+
+yay -S figlet --needed
+yay -S bitwarden --needed
+yay -S gnome-keyring
+yay -S solaar --needed
+yay -S tlpui --needed
+yay -S tlp --needed
+yay -S tlp-rdw --needed
+yay -S pavucontrol --needed
+yay -S xclip --needed
+yay -S hledger --needed
+yay -S calibre --needed
+yay -S fzf --needed
+yay -S timeshift --needed
+yay -S remmina --needed
+yay -S wireguard-tools
+yay -S timeshift
+yay -S hyprshot
+yay -S hyprpicker
+yay -S freerdp
+
+yay -S fprintd
+# git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme
+# cd theme
+# ./install.sh
